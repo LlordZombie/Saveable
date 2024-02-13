6ed4b2acace1a5f0958844ffffafc2f53cb9d005
LlordZombie <linus.freistetter@hotmail.com>
2024-02-13 22:50:42 +0100

    a schaß is des, memory oida

--- src/Media/UsbStick.java
+++ src/Media/UsbStick.java
@@ -1,19 +1,28 @@
 package Media;
 
 public class UsbStick extends MediumBeschreibbar {
-    private int schreibzyklen;
+    private final int schreibzyklen;
+    private int opsDone = 0;
 
-    public UsbStick(long length) {
+    public UsbStick(long length, int schreibzyklen) {
         super(length, 1 / Math.pow(10, 15));
+        this.schreibzyklen = schreibzyklen;
     }
 
     public void schreiben(long start, byte[][] datenNeu) {
         super.schreiben(start, datenNeu);
-
+        opsDone++;
+        if (opsDone >= schreibzyklen) {
+            bitfehlerrate += bitfehlerrate < 1 ? 0.1 : 0;
+        }
     }
 
     @Override
     public byte[][] lesen(long start, long lng) {
+        opsDone++;
+        if (opsDone >= schreibzyklen) {
+            bitfehlerrate += bitfehlerrate < 1 ? 0.1 : 0;
+        }
         return super.lesen(start, lng);
     }
 
